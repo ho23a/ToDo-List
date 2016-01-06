8411072541bd6fdc25b30bb90d6fd92201063e11
Demo User <demouser@n238-244.mtholyoke.edu>
2016-01-06 10:45:55 -0500

    Added a first model for Item.

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,5 +1,7 @@
-from __future__ import unicode_literals
-
+# from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+# models.Model extension gives Item the methods
+class Item(models.Model):
+    # '' when an item first created
+    text = models.TextField(default='')
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,7 +2,9 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
+from lists.models import Item
 from lists.views import home_page
+
 class HomePageTest(TestCase):
 
     # test home page's url set up correctly
@@ -38,7 +40,28 @@ class HomePageTest(TestCase):
             'home.html', { 'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
-        
+
 # class SmokeTest(TestCase):
 #     def test_bad_maths(self):
 #         self.assertEqual(1 + 1, 3)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        # get 2 new list items
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        # get the list of all saved items
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
