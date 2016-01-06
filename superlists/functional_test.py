6e37a729ec385a21b798a35148859b5171d38d6c
Demo User <demouser@n238-244.mtholyoke.edu>
2016-01-06 10:45:39 -0500

    Refactored fucntional test.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -14,19 +14,29 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # refactoring, help function
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    # refactoring, help function
+    def enter_a_new_item(self, item_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
 
     # test to verify the behavior
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app
         # She goes to check out its homepage
-
         # reference to firefox
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        #assert 'To-Do' in browser.title
+        # assert 'To-Do' in browser.title
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
@@ -35,8 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         #    throw new AssertionError
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
         # placeholder hints what to put in the inputbox
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -44,19 +54,16 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a test box
         # (Edith's hobby is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        self.enter_a_new_item('Buy peacock feathers')
 
         # When she hits enter, the page updates , and now the page lists
         # "1. Buy peacock feathers" as an item in a to-do lists
-        inputbox.send_keys(Keys.ENTER)
 
         # wait 15 seconds to read error
         # import time
         # time.sleep(15)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
         # self.assertTrue(
         #     # quick and dirty way
         #     any(row.text == '1. Buy peacock feathers' for row in rows),
@@ -66,8 +73,11 @@ class NewVisitorTest(unittest.TestCase):
         # There's still a text box inviting her to add another item
         # She enters 'Use peacock feathers to make fly'
         # (Edith is very methodolical)
+        self.enter_a_new_item('Use peacock feathers to make fly')
 
         # The homepage updates again and now shows both items on her lists
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('2. Use peacock feathers to make fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
