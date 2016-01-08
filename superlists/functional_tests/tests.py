aa211dc3cdfdea83d5b163cdf5cebc3ef8c0a8f5
Demo User <demouser@n238-40.mtholyoke.edu>
2016-01-08 14:48:49 -0500

    Start layout tests.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -109,6 +109,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.set_window_size(1024,768)
+        self.browser.get(self.live_server_url)
+
+        # She notices the input box is nicely centered
+        self.check_input_box_is_centered()
+
+        #She starts a new list and sees the box is centered
+        self.enter_a_new_item('testing')
+        self.check_input_box_is_centered()
+
+    def check_input_box_is_centered(self):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + (inputbox.size['width'] / 2),
+            512,
+            delta=5 # errors
+        )
+
         # Satisfied, they both go back to sleep
 
         # ______ DELETED _______
