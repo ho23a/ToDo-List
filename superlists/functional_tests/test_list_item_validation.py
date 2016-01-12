c57825a007c2b0eb6f65e389e77ed890dba6dc60
Demo User <demouser@n238-40.mtholyoke.edu>
2016-01-12 10:30:25 -0500

    Filled out the validation FT.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -2,25 +2,35 @@ from unittest import skip
 from .base import TodoFunctionalTest
 
 class ItemValidationTest(TodoFunctionalTest):
-    @skip("Haven't implemented this")
+    # @skip("Haven't implemented this")
     def test_cannot_add_empty_list_item(self):
         # Edith goes to the homepage, and accidentally tries
         # to submit an empty list item.
         # She hits Enter on the empty input box
-
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('')
+  
         # The home page refreshes, adn there is an error message
         # saying that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item,
         # which now works
+        self.enter_a_new_item('Buy milk')
+        self.check_for_row_in_list_table('1. Buy milk')
 
-        # Perversely tries to enter a second blank items
+        # Perversely, she tries to enter a second blank items
+        self.enter_a_new_item('')
 
         # She receives a similar warning on the list page_text
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
-        self.fail('Finish the test!')
-
+        self.enter_a_new_item('Make tea')
+        self.check_for_row_in_list_table('1. Buy milk')
+        self.check_for_row_in_list_table('2. Make tea')
 
         # Satisfied, they both go back to sleep
 
